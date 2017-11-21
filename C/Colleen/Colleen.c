731fb4f1eff11d0d587b0463a696d6018adb0e74
Alexandre MARRE <xSkyZie@gmail.com>
2017-11-21 11:29:39 +0100

    Update

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,4 +1,7 @@
-int main(void)
-{
-	return 0;
+#include <stdio.h>
+//commentaire1
+char *getA() {return "#include <stdio.h>%c//commentaire1%cchar* getA() {return %c%s%c;}%cint main(){%c//commentaire2%cprintf(getA(), 10, 10, 10, 34, getA(), 34, 10, 10, 10);%c}";}
+int main(){
+//commentaire2
+printf(getA(), 10, 10, 34, getA(), 34, 10, 10, 10, 10);
 }
