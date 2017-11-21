68780a6650b266cd4dda02b218213fc09dfed20d
Alexandre MARRE <xSkyZie@gmail.com>
2017-11-21 11:49:44 +0100

    Collen C done

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-//commentaire1
-char *getA() {return "#include <stdio.h>%c//commentaire1%cchar* getA() {return %c%s%c;}%cint main(){%c//commentaire2%cprintf(getA(), 10, 10, 10, 34, getA(), 34, 10, 10, 10);%c}";}
+/*
+	commentaire1
+*/
+char *getA() { return "#include <stdio.h>%c/*%c%ccommentaire1%c*/%cchar *getA() { return %c%s%c; }%cint main(){%c/*%c%ccommentaire2%c*/%cprintf(getA(), 10, 10, 9, 10, 10, 34, getA(), 34, 10, 10, 10, 9, 10, 10, 10);%c}"; }
 int main(){
-//commentaire2
-printf(getA(), 10, 10, 34, getA(), 34, 10, 10, 10, 10);
+/*
+	commentaire2
+*/
+printf(getA(), 10, 10, 9, 10, 10, 34, getA(), 34, 10, 10, 10, 9, 10, 10, 10);
 }
