d7b63c248118423d67c0740fa8c5e4421eeece47
Alexandre MARRE <xSkyZie@gmail.com>
2017-11-21 11:53:21 +0100

    Update grace

---
+++ C/Grace/Grace.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+	commentaire1
+*/
+char *getA() { return "#include <stdio.h>%c/*%c%ccommentaire1%c*/%cchar *getA() { return %c%s%c; }%cint main(){%c/*%c%ccommentaire2%c*/%cprintf(getA(), 10, 10, 9, 10, 10, 34, getA(), 34, 10, 10, 10, 9, 10, 10, 10);%c}"; }
+int main()
+{
+	/*
+	commentaire2
+*/
+	printf(getA(), 10, 10, 9, 10, 10, 34, getA(), 34, 10, 10, 10, 9, 10, 10, 10);
+}
