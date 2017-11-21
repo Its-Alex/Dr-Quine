6026756e529180650288b716006db7b60cf6f813
ItsAlex <me@itsalex.fr>
2017-11-21 20:13:13 +0100

    Add Grace

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
+#define ONE() int main() { char *a = "#include <stdio.h>%1$c#define ONE() int main() { char *a = %2$c%3$s%2$c; FILE *f; f = fopen(%2$cGrace_kid.c%2$c, %2$cw+%2$c); if (f) { fprintf(f, a, 10, 34, a); fclose(f); }}%1$c#define TWO 1%1$c#define THREE 2%1$c/*%1$c** FOUR%1$c*/%1$cONE()%1$c"; FILE *f; f = fopen("Grace_kid.c", "w+"); if (f) { fprintf(f, a, 10, 34, a); fclose(f); }}
+#define TWO 1
+#define THREE 2
 /*
-	commentaire1
+** FOUR
 */
-char *getA() { return "#include <stdio.h>%c/*%c%ccommentaire1%c*/%cchar *getA() { return %c%s%c; }%cint main(){%c/*%c%ccommentaire2%c*/%cprintf(getA(), 10, 10, 9, 10, 10, 34, getA(), 34, 10, 10, 10, 9, 10, 10, 10);%c}"; }
-int main()
-{
-	/*
-	commentaire2
-*/
-	printf(getA(), 10, 10, 9, 10, 10, 34, getA(), 34, 10, 10, 10, 9, 10, 10, 10);
-}
+ONE()
